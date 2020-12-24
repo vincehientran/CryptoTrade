75a6d945355cf45b74c47d5a280e594ce1394eb3
vincehientran <35750052+vincehientran@users.noreply.github.com>
2020-12-23 18:57:02 -0600

    Revert "connect ws"
    This reverts commit 4f5f0fbe0a7238419fb3d86f774af01636c0fdad.

--- app/src/main/java/com/vinnick/cryptotrade/MainActivity.java
+++ app/src/main/java/com/vinnick/cryptotrade/MainActivity.java
@@ -3,76 +3,12 @@ package com.vinnick.cryptotrade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.WebSocket;
-import okhttp3.WebSocketListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private TextView text;
-
-    private OkHttpClient client;
-
-    private final class EchoWebSocketListener extends WebSocketListener {
-        private static final int NORMAL_CLOSURE_STATUS = 1000;
-
-        @Override
-        public void onOpen(WebSocket webSocket, Response response) {
-            /*webSocket.send("{\n" +
-                    "    \"type\": \"subscribe\",\n" +
-                    "    \"product_ids\": [\n" +
-                    "        \"BTC-USD\"\n" +
-                    "    ],\n" +
-                    "    \"channels\": [\n" +
-                    "        \"level2\"\n" +
-                    "    ]\n" +
-                    "}");*/
-            updateText("WS opened");
-        }
-
-        @Override
-        public void onMessage(WebSocket webSocket, String text) {
-            Log.d(TAG, "onMessage: " + text);
-            updateText(text);
-        }
-
-        @Override
-        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
-            Log.d(TAG, "onFailure: ");
-        }
-
-        @Override
-        public void onClosing(WebSocket webSocket, int code, String reason) {
-            webSocket.close(NORMAL_CLOSURE_STATUS, null);
-            updateText("WS Closed");
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.textView);
-
-
-        Request request = new Request.Builder().url("wss://ws-feed.pro.coinbase.com").build();
-        EchoWebSocketListener listener = new EchoWebSocketListener();
-
-        client = new OkHttpClient();
-        WebSocket ws = client.newWebSocket(request, listener);
-
-        client.dispatcher().executorService().shutdown();
-    }
-
-    public void updateText(String message){
-        text.setText(message);
     }
 }
