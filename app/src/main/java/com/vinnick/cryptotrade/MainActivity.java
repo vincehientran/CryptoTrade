7900a5c100282ca2303cf3f2d7f5502097f7a4bf
vincehientran <35750052+vincehientran@users.noreply.github.com>
2020-12-23 19:37:34 -0600

    Revert "add async"
    This reverts commit 1e0cb2484580db569760e257cf701350bf1dfbd3.

--- app/src/main/java/com/vinnick/cryptotrade/MainActivity.java
+++ app/src/main/java/com/vinnick/cryptotrade/MainActivity.java
@@ -3,60 +3,12 @@ package com.vinnick.cryptotrade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.textView);
-
-
-        new TestAsync(this).execute();
-        /*
-        try {
-            // open websocket
-            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("wss://ws-feed.pro.coinbase.com"));
-
-            // add listener
-            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-                public void handleMessage(String message) {
-                    //updateText(message);
-                }
-            });
-
-            // send message to websocket
-            clientEndPoint.sendMessage("{\n" +
-                    "    \"type\": \"subscribe\",\n" +
-                    "    \"product_ids\": [\n" +
-                    "        \"BTC-USD\"\n" +
-                    "    ],\n" +
-                    "    \"channels\": [\n" +
-                    "        \"level2\"\n" +
-                    "    ]\n" +
-                    "}");
-
-            // wait 5 seconds for messages from websocket
-            Thread.sleep(5000);
-
-        } catch (InterruptedException ex) {
-            System.err.println("InterruptedException exception: " + ex.getMessage());
-        } catch (URISyntaxException ex) {
-            System.err.println("URISyntaxException exception: " + ex.getMessage());
-        }*/
     }
-
-    public void updateText(String message) {
-        text.setText(message);
-    }
-
-
 }
