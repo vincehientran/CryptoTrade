1e0cb2484580db569760e257cf701350bf1dfbd3
vincehientran <35750052+vincehientran@users.noreply.github.com>
2020-12-23 19:37:24 -0600

    add async

--- app/src/main/java/com/vinnick/cryptotrade/TestAsync.java
+++ app/src/main/java/com/vinnick/cryptotrade/TestAsync.java
@@ -1,35 +1,30 @@
 package com.vinnick.cryptotrade;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.TextView;
+import android.os.AsyncTask;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class MainActivity extends AppCompatActivity {
+public class TestAsync extends AsyncTask<String, Void, String> {
 
-    private TextView text;
+    private MainActivity mainActivity;
+
+    TestAsync(MainActivity ma) {
+        mainActivity = ma;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected String doInBackground(String... strings) {
 
-        text = findViewById(R.id.textView);
-
-
-        new TestAsync(this).execute();
-        /*
         try {
             // open websocket
             final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("wss://ws-feed.pro.coinbase.com"));
 
+            /*
             // add listener
             clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
                 public void handleMessage(String message) {
-                    //updateText(message);
+                    mainActivity.updateText(message);
                 }
             });
 
@@ -44,19 +39,12 @@ public class MainActivity extends AppCompatActivity {
                     "    ]\n" +
                     "}");
 
-            // wait 5 seconds for messages from websocket
-            Thread.sleep(5000);
+             */
 
-        } catch (InterruptedException ex) {
-            System.err.println("InterruptedException exception: " + ex.getMessage());
         } catch (URISyntaxException ex) {
             System.err.println("URISyntaxException exception: " + ex.getMessage());
-        }*/
+        }
+
+        return null;
     }
-
-    public void updateText(String message) {
-        text.setText(message);
-    }
-
-
 }
