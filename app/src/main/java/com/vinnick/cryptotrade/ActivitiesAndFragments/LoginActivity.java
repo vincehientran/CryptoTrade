807182b94d25bab5e3a0ad556c1179927a1f2533
vincehientran <35750052+vincehientran@users.noreply.github.com>
2021-01-05 23:00:44 -0600

    add firebase to cryptotrade

---
+++ app/src/main/java/com/vinnick/cryptotrade/ActivitiesAndFragments/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.vinnick.cryptotrade.ActivitiesAndFragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vinnick.cryptotrade.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
