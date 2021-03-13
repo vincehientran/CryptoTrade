9eeaa13e07982f8d701bd0fee5a056baa194080b
pearltran080 <76570762+pearltran080@users.noreply.github.com>
2021-03-13 17:42:05 -0600

    remove line 16

--- app/src/main/java/com/vinnick/cryptotrade/CurrentPrice.java
+++ app/src/main/java/com/vinnick/cryptotrade/CurrentPrice.java
@@ -13,7 +13,6 @@ import java.nio.charset.Charset;
 
 public class CurrentPrice {
 
-
     private static final String API_LINK_BEGINNING = "https://api.nomics.com/v1/currencies/ticker?key=";
     //https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=BTC&interval=1d
     private static final String API_KEY = "66d6fc35ed618c1fbf359c96d88b671c";
